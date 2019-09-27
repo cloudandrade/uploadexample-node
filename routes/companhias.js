7a63954d4bbef60219e987fec21f16b36fdceb85
Jan <jan.andrade@capgemini.com>
2019-09-27 10:58:53 -0300

    feat: painel com cont dinamica, cadastro companhia

--- routes/companhias.js
+++ routes/companhias.js
@@ -16,4 +16,60 @@ router.get("/cadastro", (req, res) => {
   res.render("companhia/criar-companhia");
 });
 
+//register handle
+router.post("/cadastro", (req, res) => {
+  const { nome, descricao } = req.body;
+  let errors = [];
+
+  //check required fields
+  if (!nome) {
+    errors.push({ msg: "Companhia precisa ter um nome" });
+  }
+
+  //se houver erros recarrega a página
+  if (errors.length > 0) {
+    res.render("companhia/criar-companhia", {
+      errors,
+      nome,
+      descricao
+    });
+  } else {
+    //validation passed
+    Companhia.findOne({ nome: nome })
+      .then(companhia => {
+        if (companhia) {
+          //companhia  exists
+          errors.push({ msg: "Já existe um companhia com esse nome" });
+          res.render("companhia/criar-companhia", {
+            errors,
+            nome,
+            descricao
+          });
+        } else {
+          const novaCompanhia = new Companhia({
+            nome,
+            descricao
+          });
+
+          Companhia.count({}, function(err, count) {
+            console.log("Number of docs: ", count);
+          });
+
+          novaCompanhia
+            .save()
+            .then(companhiaCriada => {
+              req.flash("success_msg", "Companhia Criada com sucesso");
+              res.redirect("/dashboard");
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,20 @@ const staticRoutes = require("./static");
 const jovensRoutes = require("./jovens");
 const companhiaRoutes = require("./companhias");
 const consultorRoutes = require("./consultor");
+const mongoose = require("mongoose");
+
+//models
+require("../models/Companhia");
+const Companhia = mongoose.model("companhias");
+
+require("../models/Jovem");
+const Jovem = mongoose.model("jovens");
+
+require("../models/Consultor");
+const Consultor = mongoose.model("consultores");
+
+require("../models/User");
+const Users = mongoose.model("users");
 
 //Routes
 router.get("/", (req, res) => res.render("index"));
@@ -15,10 +29,24 @@ router.use("/dashboard/jovens", jovensRoutes);
 router.use("/dashboard/companhias", companhiaRoutes);
 router.use("/dashboard/consultores", consultorRoutes);
 
-router.get("/dashboard", ensureAuthenticated, (req, res) =>
+router.get("/dashboard", ensureAuthenticated, async (req, res) => {
+  const companhiaCount = await Companhia.count({});
+  const jovemCount = await Jovem.count({});
+  const consultorCount = await Consultor.count({});
+  const usersCount = await Users.count({});
+
+  console.log(usersCount);
+  console.log(companhiaCount);
+  console.log(consultorCount);
+  console.log(jovemCount);
+
   res.render("home", {
-    name: req.user.name
-  })
-);
+    name: req.user.name,
+    userscont: usersCount,
+    companhiacont: companhiaCount,
+    jovemcont: jovemCount,
+    consultorcont: consultorCount
+  });
+});
 
 module.exports = router;
